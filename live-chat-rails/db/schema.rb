765f1814ac0ab75fd61bc823320e44ac84cf66a2
Yoshiki Hasumi <yoshiki.h.105@gmail.com>
2022-07-12 08:10:00 +0900

    add: Messageモデル

--- live-chat-rails/db/schema.rb
+++ live-chat-rails/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_09_011352) do
+ActiveRecord::Schema.define(version: 2022_07_11_230736) do
+
+  create_table "messages", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
@@ -25,4 +33,5 @@ ActiveRecord::Schema.define(version: 2022_07_09_011352) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "messages", "users"
 end
